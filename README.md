f20fcec29acaf6fe993f67c79fe43f525e3d3e2c
Gianluca Lofrumento <gianluca.lofrumento@gmail.com>
2020-05-04 01:07:07 +0200

    Argument Parsing added

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # Mavic Pro conversion tool DNG to PNG
 
-Conversion tool for DNG rawfiles created by Mavic Pro into PNG files.
+Conversion tool for DNG rawfiles  Mavic Pro into PNG files.
 
 ## Installing requirements
 
-It's required python version ≥ 3 and pip. Install then the requirements:
+It's required python version ≥ 3 and pip. Install then the required packages:
 
 ```bash
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 ```
 
-## Execution
+## Usage
 
-1. Put the script inside the folder in which images are
-2. Run ``` python3 conver.py ```
+You have two ways to run this script.
 
-The script will create a directory *PNG* in the folder path and inside will put all the photos converted in PNG extension.
+Either put *convert.py* into the folder where DNG files are and then run ```python3 convert.py```. It will be asked you to specify the destination, whose default value is *./PNG* .
+
+Otherwise, you can specify a source path (and a destination path) running ```python3 convert.py --source /source/path --destination /destination/path```. If the destination path does not exists, it will be asked you to confirm to create it. If you don't specify it, its deafult value is again */source/path/PNG* .
+
+Enjoy.
+
+[Follow me!](https://github.com/glofru)
+
--- convert.py
+++ convert.py
@@ -1,16 +1,57 @@
+import argparse
 import glob
-import imageio
 import os
+import sys
+
+import imageio
 import rawpy
 
-if not os.path.exists('./PNG'):
-    os.mkdir('PNG')
+parser = argparse.ArgumentParser(description='Tool for DNG to PNG conversion.')
 
-files = glob.glob(str(os.getcwd()) + '/*.DNG')
+parser.add_argument('--source', '-s', metavar='', help='Folder where DNG files are')
+parser.add_argument('--destination', '-d', metavar='', help='Folder where PNG files are saved')
+
+args = parser.parse_args()
+
+if args.source is None:
+    source = str(os.getcwd())
+    print("Source path is current path " + source)
+else:
+    source = str(args.source)
+    if not os.path.exists(source):
+        sys.exit(args.source + ' path not found')
+
+if args.destination is None:
+    dest = input('Where do you want to save your PNG files? [' + source + '/PNG]: ')
+    if len(dest) == 0:
+        dest = source + '/PNG'
+else:
+    dest = str(args.destination)
+
+if not os.path.exists(dest):
+    if dest != source + '/PNG':
+        b = input(dest + ' does not exists. Do you want to create it? [yes]: ')
+
+        if len(b) != 0 and b.lower()[0] == 'n':
+            sys.exit()
+
+    os.makedirs(dest)
+
+files = glob.glob(source + '/*.DNG')
+
+if len(files) == 0:
+    sys.exit('No DNG files found in ' + source)
+
+count = 0
+
+print('Elaborating...')
 
 for i in files:
     raw = rawpy.imread(i)
     rgb = raw.postprocess()
-    newpath = i[:len(i)-12] + 'PNG/' + i[len(i)-12:len(i)-3] + 'png'
+    newpath = dest + '/' + i.split('/')[-1][:-3] + 'png'
     imageio.imwrite(newpath, rgb)
-    print(newpath)
+    count += 1
+    print(str(int(count*100/len(files))) + '% ' + str(count) + '/' + str(len(files)) + ': ' + newpath)
+
+print('Job finished!')
