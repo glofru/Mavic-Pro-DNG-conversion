a8207f68b79fc96cec2591449c74cd4c53dc8840
Gianluca Lofrumento <gianluca.lofrumento@gmail.com>
2020-05-05 22:37:42 +0200

    Added JPG to TIFF conversion

--- convert.py
+++ convert.py
@@ -5,11 +5,14 @@ import sys
 
 import imageio
 import rawpy
+from PIL import Image
 
 parser = argparse.ArgumentParser(description='Tool for DNG to PNG conversion.')
 
 parser.add_argument('--source', '-s', metavar='', help='Folder where DNG files are')
 parser.add_argument('--destination', '-d', metavar='', help='Folder where PNG files are saved')
+
+parser.add_argument('--jpg-to-tiff', '-j', action='store_true', help='Variation to convert JPG files to TIFF files')
 
 args = parser.parse_args()
 
@@ -22,14 +25,14 @@ else:
         sys.exit(args.source + ' path not found')
 
 if args.destination is None:
-    dest = input('Where do you want to save your PNG files? [' + source + '/PNG]: ')
+    dest = input('Where do you want to save your files? [' + source + '/convert]: ')
     if len(dest) == 0:
-        dest = source + '/PNG'
+        dest = source + '/convert'
 else:
     dest = str(args.destination)
 
 if not os.path.exists(dest):
-    if dest != source + '/PNG':
+    if dest != source + '/convert':
         b = input(dest + ' does not exists. Do you want to create it? [yes]: ')
 
         if len(b) != 0 and b.lower()[0] == 'n':
@@ -37,21 +40,36 @@ if not os.path.exists(dest):
 
     os.makedirs(dest)
 
-files = glob.glob(source + '/*.DNG')
-
-if len(files) == 0:
-    sys.exit('No DNG files found in ' + source)
-
 count = 0
 
 print('Elaborating...')
 
-for i in files:
-    raw = rawpy.imread(i)
-    rgb = raw.postprocess()
-    newpath = dest + '/' + i.split('/')[-1][:-3] + 'png'
-    imageio.imwrite(newpath, rgb)
-    count += 1
-    print(str(int(count*100/len(files))) + '% ' + str(count) + '/' + str(len(files)) + ': ' + newpath)
+if args.jpg_to_tiff:
+    files = glob.glob(source + '/*.jpg', source + '/*.jpeg', source + '/*.JPG', source + '/*.JPEG')
+
+    if len(files) == 0:
+        sys.exit('No JPG files found in ' + source)
+
+    for i in files:
+        im = Image.open(i)
+        newpath = dest + '/' + i.split('/')[-1][:-3] + 'tiff'
+        im.save(newpath)
+        count += 1
+        print(str(int(count*100/len(files))) + '% ' + str(count) + '/' + str(len(files)) + ': ' + newpath)
+
+
+else:
+    files = glob.glob(source + '/*.DNG', source + '/*.dng')
+
+    if len(files) == 0:
+        sys.exit('No DNG files found in ' + source)
+
+    for i in files:
+        raw = rawpy.imread(i)
+        rgb = raw.postprocess()
+        newpath = dest + '/' + i.split('/')[-1][:-3] + 'png'
+        imageio.imwrite(newpath, rgb)
+        count += 1
+        print(str(int(count*100/len(files))) + '% ' + str(count) + '/' + str(len(files)) + ': ' + newpath)
 
 print('Job finished!')
